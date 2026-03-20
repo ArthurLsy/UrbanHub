adaa0ed2c815698983eeb00d767efbedff2c746c
Felixjuteau <felixjuteau@gmail.com>
2026-03-20 11:06:40 +0100

    first commit

---
+++ src/main/java/caensup/eadl/urbanhub/UrbanHubApplication.java
@@ -0,0 +1,13 @@
+package caensup.eadl.urbanhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanHubApplication.class, args);
+	}
+
+}
